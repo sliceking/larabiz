756df401cf179fba3a2d9849842d15be18063b62
Stan Wielga <svwielga4@gmail.com>
2019-12-26 10:57:28 -0800

    auth added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <h3>Your Listings</h3>
+                    @if (count($listings))
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Company</th>
+                            </tr>
+                            @foreach ($listings as $listing)
+                            <tr>
+                                <td>{{ $listing->name }}</td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You don't have any listings yet</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
